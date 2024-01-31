eadd5ff62d85526fcc10d356d59a9fae4063d105
SaintHazzard <chamitos1998@hotmail.com>
2024-01-31 07:57:58 -0500

    feat: :sparkles: Menu desplegable progresivo
    Se configura el meno desplegable para el apartado movil

--- storage/js/script.js
+++ storage/js/script.js
@@ -6,6 +6,13 @@ function togleMenu() {
   menu = document.querySelector('.menu')
   h1header = document.querySelector('.h1header')
 
-  h1header.style.display = (h1header.style.display === 'block') ? 'none' : 'block';
+  // h1header.style.display = (h1header.style.display === 'block') ? 'none' : 'block';
   menu.classList.toggle('menuMovil')
 }
+
+// document.getElementById('toggleMenu').addEventListener('click', function () {
+//   var menu = document.querySelector('.menu');
+//   menu.classList.toggle('active');
+// });
+
+// console.log('entra')
